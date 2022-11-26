7e5d1898fda1cff50707da26fe6009fa203da934
Fayssal Chokri <ahedmarouane@proton.me>
2022-11-26 05:38:40 -0800

    Property fixes.

---
+++ amiga/workbench/hidds/graphics/intuition/include/graphics-amiga-intuition.h
@@ -0,0 +1,18 @@
+#ifndef HIDD_GRAPHICS_AMIGA_INTUI_H
+#define HIDD_GRAPHICS_AMIGA_INTUI_H
+#ifndef EXEC_TYPES_H
+#   include <exec/types.h>
+#endif
+#ifndef HIDD_HIDD_H
+#   include <hidd/hidd.h>
+#endif
+#ifndef OOP_OOP_H
+#   include <oop/oop.h>
+#endif
+
+#include <utility/utility.h>
+
+#define CLID_Hidd_GfxAmigaIntui    "graphics_amiga_intui.hidd"
+#define CLID_Hidd_BitMapAmigaIntui "bitmapamigaintui.hidd"
+
+#endif /* HIDD_GRAPHICS_AMIGA_INTUI_H */
