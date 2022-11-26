aec0a0a29fd54ed8b12aa37286a3becde713e409
Fayssal Chokri <ahedmarouane@proton.me>
2022-11-26 08:12:15 -0800

    Initial revision.

---
+++ dummy/cachecontrol.c
@@ -0,0 +1,42 @@
+|*****************************************************************************
+|
+|   NAME
+|
+|	__easyos_LH2(ULONG, CacheControl,
+|
+|   SYNOPSIS
+|	__easyos_LA(ULONG, cacheBits, D0),
+|	__easyos_LA(ULONG, cacheMask, D1),
+|
+|   LOCATION
+|	struct ExecBase *, SysBase, 108, Exec)
+|
+|   FUNCTION
+|	Change/read the values in the 68030 cacr register. Only the bits set
+|	in the mask parameter are affected.
+|
+|   INPUTS
+|	cacheBits - new bit values.
+|	cacheMask - Bits to change.
+|
+|   RESULT
+|	Old contents of cacr register.
+|
+|   NOTES
+|
+|   EXAMPLE
+|
+|   BUGS
+|
+|   SEE ALSO
+|
+|   INTERNALS
+|
+|   HISTORY
+|
+|******************************************************************************
+
+	| Simple 68000s have no chaches
+	.globl	_Exec_CacheControl
+_Exec_CacheControl:
+	rts
