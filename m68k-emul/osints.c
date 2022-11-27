3f6b0684a8fa74179ef2d60d5041e21975e236d6
Fayssal Chokri <ahedmarouane@proton.me>
2022-11-27 05:06:18 -0800

    Initial revision.

---
+++ m68k-emul/osints.c
@@ -0,0 +1,26 @@
+#include <signal.h>
+#include <stdio.h>
+
+/* The real functions are written in assembler as stubs which call these */
+void _os_enable (void)
+{
+    sigset_t set;
+
+    /* if (supervisor)
+    {
+        fprintf (stderr, "Enable() called in supervisor mode\n");
+    } */
+
+    sigfillset (&set);
+
+    sigprocmask (SIG_UNBLOCK, &set, NULL);
+} /* _os_enable */
+
+void _os_disable (void)
+{
+    sigset_t set;
+
+    sigfillset (&set);
+
+    sigprocmask (SIG_BLOCK, &set, NULL);
+} /* _os_disable */
