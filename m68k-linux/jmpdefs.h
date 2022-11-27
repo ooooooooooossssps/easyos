4765125048d9a858ae084ccb0a01deee517ff904
Fayssal Chokri <107705927+fayssalchokrii@users.noreply.github.com>
2022-11-27 06:15:38 -0800

    update
    Co-Authored-By: Fayssal Chokri <107705927+fayssalchokrii@users.noreply.github.com>

---
+++ m68k-linux/jmpdefs.h
@@ -0,0 +1,24 @@
+#ifndef JMPDEFS_H
+#define JMPDEFS_H
+
+#include <easyos/system.h>
+#include <setjmp.h>
+
+#if defined(__GLIBC__) && (__GLIBC__ >= 2)
+#  define SP(env)	((APTR)(env[0].__jmpbuf[0].__sp))
+#  define FP(env)	((APTR)(env[0].__jmpbuf[0].__fp))
+#  define PC(env)	((APTR)(env[0].__jmpbuf[0].__pc))
+#else
+#  define SP(env)	((APTR)(env[0].__sp))
+#  define FP(env)	((APTR)(env[0].__fp))
+#  define PC(env)	((APTR)(env[0].__pc))
+#endif
+
+/* The number of stack longs that have to be copied from the old stack */
+#if UseRegisterArgs
+#  define NUM_LONGS	6
+#else
+#  define NUM_LONGS	4
+#endif
+
+#endif /* !JMPDEFS_H */
